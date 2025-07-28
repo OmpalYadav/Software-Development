dea020c33b54e7c2b6a316bcf58e78ed9be51137
Jai Shree Ram🧡 <ompalyadav2235@gmail.com>
2025-07-28 13:53:59 +0530

    Initial upload of full Frontend project

---
+++ HTML/Project1/script.js
@@ -0,0 +1,1 @@
+alert("Hello Ompal Welcome Back");
---
+++ JAVASCRIPT/data.js
@@ -0,0 +1,65 @@
+// We learn in this Code Datatype of Js
+
+
+let Name="Ompal Yadav"; //  String
+let age=25; // Interger
+let Id= true; // Boolean
+let address; //Null (Undefined) StandAlone Value
+// Underfined datatype
+// Symbol=> unique
+// object
+
+
+console.log(Name);
+console.log(age);
+console.log(Id);
+console.log(address);
+
+console.log(typeof Name);
+console.log(typeof age);
+console.log(typeof Id);
+console.log(typeof address);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+//Type Conversion in JavaScript
+
+ // string to number
+let score = "Ram";
+console.log(score);
+console.log(typeof score);
+console.log(typeof (score));
+
+let Valuenumber=Number(score);
+console.log(typeof Valuenumber);
+console.log(Valuenumber);
+
+//string to boolean
+
+let Vbool= "Raj";
+console.log(Vbool);
+console.log(typeof Vbool);
+let Valueboolean=Boolean(Vbool);
+console.log(typeof Valueboolean);
+console.log(Valueboolean);
+
+// Number to String
+let VNumber= 33;
+console.log(VNumber);
+console.log(typeof VNumber);
+let VaNumber=String(VNumber);
+console.log(typeof VaNumber);
+console.log(VaNumber);
+
+// Number to Boolean
+let Vbooll= true;
+console.log(Vbooll);
+console.log(typeof Vbooll);
+let Vabool=Number(Vbooll);
+console.log(typeof Vabool);
+console.log(Vabool);
+
+
+
+
