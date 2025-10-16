951315c0fa8eb63059d5b3e6d4c1471424f610ce
Ompal Yadav <ompalyadav.dev@gmail.com>
2025-10-16 17:05:40 +0530

    Javascript

---
+++ JAVASCRIPT/variable.js
@@ -0,0 +1,15 @@
+let firstname = "Ompal Yadav"
+const AccountNumber = 123244
+let AccountEmail = "Ompalyadav2235@gmail.com"
+city="Agra"
+let state
+//AccountNuwmber=6545565455 not change we are not use var because of block scope//
+firstname="Yadavk"
+AccountEmail="ompalyadav@google.com"
+
+console.log(firstname)
+console.log(AccountNumber)
+console.log(AccountEmail)
+
+console.table([firstname,AccountNumber,AccountEmail,city,state])
+
